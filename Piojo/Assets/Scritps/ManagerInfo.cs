7eb5889b1a25b61d87d06ab0290cdfdb428e4cec
JuanJesus5 <61622894+JuanJesus5@users.noreply.github.com>
2021-10-22 19:13:40 +0200

    Info de todos los elementos

--- Piojo/Assets/Scritps/ManagerInfo.cs
+++ Piojo/Assets/Scritps/ManagerInfo.cs
@@ -5,22 +5,77 @@ using UnityEngine.UI;
 
 public class ManagerInfo : MonoBehaviour
 {
-    //Mensaje
-    public GameObject DescpEnfermera;
+    //Mejoras de Generación
+    public GameObject InfoEnfermera;
+    public GameObject InfoPMadre;
+    public GameObject InfoEscuela;
+    public GameObject InfoHostal;
+    public GameObject InfoManosMagicas;
+    public GameObject InfoJetpack;
+
+    //Mejoras de Combate
+    public GameObject InfoPiojoMazao;
+    public GameObject InfoCazaTitanes;
+    public GameObject InfoPiojoCanon;
+    public GameObject InfoPiojoMan;
 
     // Start is called before the first frame update
     void Start()
     {
-        DescpEnfermera.SetActive(false);
+        //Mejoras de Generación
+        InfoEnfermera.SetActive(false);
+        InfoPMadre.SetActive(false);
+        InfoEscuela.SetActive(false);
+        InfoHostal.SetActive(false);
+        InfoManosMagicas.SetActive(false);
+        InfoJetpack.SetActive(false);
+
+        //Mejoras de Combate
+        InfoPiojoMazao.SetActive(false);
+        InfoCazaTitanes.SetActive(false);
+        InfoPiojoCanon.SetActive(false);
+        InfoPiojoMan.SetActive(false);
+
     }
 
-    public void OnMouseOver()
-    {
-        DescpEnfermera.SetActive(true);
-    }
-    public void OnMouseExit()
-    {
-        DescpEnfermera.SetActive(false);
-    }
+    //Enfermera
+    public void OnMouseOverEnfermera(){InfoEnfermera.SetActive(true);}
+    public void OnMouseExitEnfermera(){InfoEnfermera.SetActive(false);}
+
+    //Piojo Madre
+    public void OnMouseOverPiojoMadre() { InfoPMadre.SetActive(true); }
+    public void OnMouseExitPiojoMadre() { InfoPMadre.SetActive(false); }
+
+    //Escuela
+    public void OnMouseOverEscuela() { InfoEscuela.SetActive(true); }
+    public void OnMouseExitEscuela() { InfoEscuela.SetActive(false); }
+
+    //Hostal
+    public void OnMouseOverHostal() { InfoHostal.SetActive(true); }
+    public void OnMouseExitHostal() { InfoHostal.SetActive(false); }
+
+    //Manos Mágicas
+    public void OnMouseOverManosM() { InfoManosMagicas.SetActive(true); }
+    public void OnMouseExitManosM() { InfoManosMagicas.SetActive(false); }
+
+    //Jetpack
+    public void OnMouseOverJetpack() { InfoJetpack.SetActive(true); }
+    public void OnMouseExitJetpack() { InfoJetpack.SetActive(false); }
+
+    //PiojoMazao
+    public void OnMouseOverPMazao() { InfoPiojoMazao.SetActive(true); }
+    public void OnMouseExitPMazao() { InfoPiojoMazao.SetActive(false); }
+
+    //CazaTitanes
+    public void OnMouseOverCazaTitanes() { InfoCazaTitanes.SetActive(true); }
+    public void OnMouseExitCazaTitanes() { InfoCazaTitanes.SetActive(false); }
+
+    //PiojoCanon
+    public void OnMouseOverPCanon() { InfoPiojoCanon.SetActive(true); }
+    public void OnMouseExitPCanon() { InfoPiojoCanon.SetActive(false); }
+
+    //PiojoMan
+    public void OnMouseOverPiojoMan() { InfoPiojoMan.SetActive(true); }
+    public void OnMouseExitPiojoMan() { InfoPiojoMan.SetActive(false); }
 
 }

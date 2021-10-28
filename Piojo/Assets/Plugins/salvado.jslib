11d24004ee150059d3f8c22886fb73f8160e1e35
M-Galvez <m.galvez.2018@alumnos.urjc.es>
2021-10-28 03:47:17 +0200

    Clase Partida y archivos de apoyo

---
+++ Piojo/Assets/Plugins/salvado.jslib
@@ -0,0 +1,18 @@
+mergeInto(LibraryManager.library, {
+
+  GuardarDatos: function (datos) {
+    d=Pointer_stringify(datos);
+    datos=JSON.stringify(d);
+    window.localStorage.setItem('datos',d);
+  },
+
+  CargarDatos: function () {
+    s=window.localStorage.getItem('datos');
+    
+    var bufferSize = lengthBytesUTF8(s) + 1;
+   var buffer = _malloc(bufferSize);
+   stringToUTF8(s, buffer, bufferSize);
+   return buffer;
+  }
+
+});
---
+++ Piojo/Assets/Scritps/Partida.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Partida
+{
+    public int numPiojos { get; set; }
+    public List<Enemigo> listaEnemigos{ get; set; }
+    public int costePMadre { get; set; }
+    public int contPiojoMadre { get; set; }
+    public int costeCabezaN { get; set; }
+    public int contCabezaN { get; set; }
+    public int costeClaseInfantil { get; set; }
+    public int contClaseInfantil { get; set; }
+    public int costePatioEscuela { get; set; }
+    public int contPatioEscuela { get; set; }
+    public int costeEscuela { get; set; }
+    public int contEscuela { get; set; }
+    public int costeCPerros { get; set; }
+    public int contCPerros { get; set; }
+    public int costeHostal { get; set; }
+    public int contHostal { get; set; }
+    public int costeJetpack { get; set; }
+    public int contJetpack { get; set; }
+
+    public int costePMazao { get; set; }
+    public int contPiojoMazao { get; set; }
+    public int costeCTitanes { get; set; }
+    public int contCazaTitanes { get; set; }
+    public int costePCanon { get; set; }
+    public int contPCanon { get; set; }
+    public int costePMan { get; set; }
+    public int contPiojoMan { get; set; }
+    public int costeJefe { get; set; }
+    public int contJefe { get; set; }
+    public int costeLiderM { get; set; }
+    public int contLiderM { get; set; }
+    public int costePEstratega { get; set; }
+    public int contPEstratega { get; set; }
+    public int costePKasparov { get; set; }
+    public int contPKasparov { get; set; }
+    public int costeEnfermera { get; set; }
+    public int contEnfermera { get; set; }
+    public int costePPagado { get; set; }
+    public int contPPagado { get; set; }
+    public int costePCafeina { get; set; }
+    public int contPCafeina { get; set; }
+    public int costePSpeed { get; set; }
+    public int contPSpeed { get; set; }
+    public int costePMellizos { get; set; }
+    public int contPMellizos { get; set; }
+    public int costePQuinti { get; set; }
+    public int contPQuinti { get; set; }
+    public int costePDeca { get; set; }
+    public int contPDeca { get; set; }
+    public int costePHecta { get; set; }
+    public int contPHecta { get; set; }
+    public int cantidadMejorPago { get; set; }
+    public int costeClonaP { get; set; }
+    public bool activeClonaP { get; set; }
+    public int costeMultiC { get; set; }
+    public bool activeMultiC { get; set; }
+    
+}

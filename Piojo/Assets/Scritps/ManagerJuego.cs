df549f25cea9cd13db2268a9aa5e20e44b238bd7
M-Galvez <m.galvez.2018@alumnos.urjc.es>
2021-12-12 19:56:01 +0100

    Arreglado random enemigos y vida enemigos

--- Piojo/Assets/Scritps/ManagerJuego.cs
+++ Piojo/Assets/Scritps/ManagerJuego.cs
@@ -1518,22 +1518,15 @@ public class ManagerJuego : MonoBehaviour
         StopCoroutine(ayuda());
     }
 
+
     //Enemigos
     private void inicializarEnemigos()
     {
 
         Random.InitState((int)Time.realtimeSinceStartup);
-        int num = Random.Range(0, 7);
+        int num = Random.Range(0, 8);
         Enemigo e1 = new Enemigo(vidaBase, (tipoEnemigo)num, barreras.SinProtecciones, num, num);
-        Random.InitState((int)Time.realtimeSinceStartup);
-        num = Random.Range(0, 7);
-        Enemigo e2 = new Enemigo((vidaBase * (enemDerrotados + 1) + e1.GetVidaMax() * factor), (tipoEnemigo)num, barreras.Casco, num, num);
-        Random.InitState((int)Time.realtimeSinceStartup);
-        num = Random.Range(0, 7);
-        Enemigo e3 = new Enemigo((vidaBase * (enemDerrotados + 1) + e2.GetVidaMax() * factor), (tipoEnemigo)num, barreras.Chaleco, num, num);
         listaEnemigos.Add(e1);
-        listaEnemigos.Add(e2);
-        listaEnemigos.Add(e3);
         barraVida.setVidaMaxima(listaEnemigos[0].GetVidaMax());
         barraVida.setVida(listaEnemigos[0].GetVidaActual());
         BtnEnemigo.sprite = spritesEnem[listaEnemigos[0].imagen];
@@ -1542,17 +1535,19 @@ public class ManagerJuego : MonoBehaviour
 
     private void recuperaVida()
     {
-        Random.InitState((int)Time.realtimeSinceStartup);
+        Random.InitState(numPiojos);
+
         int num = Random.Range(0, 10);
         if (listaEnemigos[0].GetVidaActual() <= listaEnemigos[0].GetVidaMax() / 2 && num > 8)
         {
             barraVida.setVidaMaxima(listaEnemigos[0].GetVidaMax());
-            listaEnemigos[0].SetVidaActual(listaEnemigos[0].GetVidaActual() + listaEnemigos[0].GetVidaMax() / 10);
+            listaEnemigos[0].SetVidaActual(listaEnemigos[0].GetVidaActual() + (int)(listaEnemigos[0].GetVidaMax() / 10));
             barraVida.setVida(listaEnemigos[0].GetVidaActual());
             StartCoroutine(muestraCuracion());
 
 
             vidaEnemigo.text = FormatoNum(listaEnemigos[0].GetVidaActual()) + "/" + FormatoNum(listaEnemigos[0].GetVidaMax());
+
         }
     }
 
@@ -1867,7 +1862,7 @@ public class ManagerJuego : MonoBehaviour
             if (listaEnemigos.Count == 1)
             {
                 Random.InitState((int)Time.realtimeSinceStartup);
-                int r = Random.Range(0, 7);
+                int r = Random.Range(0, 8);
                 int barrier = Random.Range(0, 5);
                 Enemigo e = new Enemigo((listaEnemigos[0].GetVidaMax() + vidaBase * (enemDerrotados + 1)) * factor, (tipoEnemigo)r, (barreras)barrier, r, r);
                 inicializarBarreras(e);
